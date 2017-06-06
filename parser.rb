3c01d20a63150758657306958b5d7a413a7daa6a
alexeybahter24 <smith.job242gmail.com>
2017-06-06 14:16:53 +0300

    Version 2

--- parser.rb
+++ parser.rb
@@ -3,18 +3,18 @@ class XmlFile
   @@line_count = []
 
   def read_xml
-    File.open('mddrive.xml').each { |line| @@line_count << line.scan(/template name="(.*)"/) }
-    @@line_count = @@line_count.uniq
-    puts @@line_count.delete_at(0)
+    File.open('mddrive.xml').each do |line|
+      if /template name/.match line
+      @@line_count << line.scan(/template name="(.*)"/)
+      end
+    end
     @@line_count = @@line_count.join("\n")
-    puts 'вызван метод read_xml'
     return @@line_count
   end
 
   def write_xml
     f = File.new('xml_file.txt', 'w')
     f.puts "#{@@line_count}"
-    puts 'вызван метод write_xml'
   end
 
 end
