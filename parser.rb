0a7baf014ef3666e4df1242c9357b4f2b5acc777
alexeybahter24 <smith.job242gmail.com>
2017-06-06 17:49:52 +0300

    Last_version

--- parser.rb
+++ parser.rb
@@ -1,24 +1,12 @@
 class XmlFile
 
-  @@line_count = []
-
-  def read_xml
-    File.open('mddrive.xml').each do |line|
-      if /template name/.match line
-      @@line_count << line.scan(/template name="(.*)"/)
-      end
-    end
-    @@line_count = @@line_count.join("\n")
-    return @@line_count
-  end
-
-  def write_xml
-    f = File.new('xml_file.txt', 'w')
-    f.puts "#{@@line_count}"
+  def read_and_write_xml(file_name='mddrive.xml')
+    file = []
+    File.open(file_name).each { |line| file << line.scan(/template name="(.*)"/) if (/template name/.match line) }
+    File.open('xml_file.txt', 'w') { |f| f.puts "#{file.join("\n")}" }
   end
 
 end
 
 x = XmlFile.new
-x.read_xml
-x.write_xml
+x.read_and_write_xml()
